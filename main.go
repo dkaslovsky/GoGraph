65f91d837e4f5631840d82034e9f230fafa3625e
dkaslovsky <dkaslovsky@gmail.com>
2019-03-24 14:30:56 -0600

    main prints adjacency

--- main.go
+++ main.go
@@ -4,61 +4,18 @@ import (
 	"fmt"
 
 	"github.com/dkaslovsky/GoGraph/graph"
+	"github.com/dkaslovsky/GoGraph/io"
 )
-
-func printNbrs(g *graph.DirGraph, node string) {
-	nbrs, ok := g.GetNeighbors(node)
-	if !ok {
-		fmt.Printf("%s not found in %s\n", node, g.Name)
-		return
-	}
-	fmt.Printf("%s's neighbors: %v\n", node, nbrs)
-}
-
-func printOutDegree(g *graph.DirGraph, node string) {
-	d, ok := g.GetOutDegree(node)
-	if !ok {
-		fmt.Printf("%s not found in %s\n", node, g.Name)
-		return
-	}
-	fmt.Printf("%s's out degree: %f\n", node, d)
-}
-
-func printInDegree(g *graph.DirGraph, node string) {
-	d, ok := g.GetInDegree(node)
-	if !ok {
-		fmt.Printf("%s not found in %s\n", node, g.Name)
-		return
-	}
-	fmt.Printf("%s's in degree: %f\n", node, d)
-}
 
 func main() {
 	g := graph.NewDirGraph("myGraph")
-	err := g.FromFile("graph.txt")
+	err := io.ReadFromFile("graph.txt", g)
 	if err != nil {
 		fmt.Printf("Error %v\n", err)
 	}
 
-	fmt.Printf("Nodes in %s: %v\n", g.Name, g.GetNodes())
-	g.PrintAdj()
-	for _, node := range []string{"a", "c", "x"} {
-		printNbrs(g, node)
-		printOutDegree(g, node)
-		printInDegree(g, node)
-	}
-
-	fmt.Println()
-
-	n1 := "c"
-	n2 := "d"
-	fmt.Printf("Removing edge (%s, %s)", n1, n2)
-	g.RemoveEdge(n1, n2)
-	fmt.Printf("Nodes in %s: %v\n", g.Name, g.GetNodes())
-	g.PrintAdj()
-	for _, node := range []string{"a", "c", "x"} {
-		printNbrs(g, node)
-		printOutDegree(g, node)
-		printInDegree(g, node)
-	}
+	fmt.Println("\nOut adjacency:")
+	g.PrintOutAdj()
+	fmt.Println("\nIn adjacency:")
+	g.PrintInAdj()
 }
