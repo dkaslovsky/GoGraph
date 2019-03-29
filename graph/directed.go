505a7653a330770c860d0ec722853547758ab426
dkaslovsky <dkaslovsky@gmail.com>
2019-03-28 21:38:07 -0600

    comments

--- graph/directed.go
+++ graph/directed.go
@@ -1,11 +1,13 @@
 package graph
 
+// DirGraph is an adjacency map representation of a directed graph
 type DirGraph struct {
 	Name   string
 	outAdj dirAdj
 	inAdj  dirAdj // inverted index of outAdj
 }
 
+// NewDirGraph creates a new directed graph
 func NewDirGraph(name string) *DirGraph {
 	return &DirGraph{
 		Name:   name,
@@ -14,6 +16,7 @@ func NewDirGraph(name string) *DirGraph {
 	}
 }
 
+// AddEdge adds an edge from a node to another node with an optional weight that defaults to 1.0
 func (dg *DirGraph) AddEdge(from string, to string, weight ...float64) {
 	wgt := 1.0
 	if len(weight) > 0 {
@@ -23,11 +26,14 @@ func (dg *DirGraph) AddEdge(from string, to string, weight ...float64) {
 	dg.inAdj.addEdge(to, from, wgt)
 }
 
+// RemoveEdge removes an edge that exists from a node to another node
 func (dg *DirGraph) RemoveEdge(from string, to string) {
 	dg.outAdj.removeEdge(from, to)
 	dg.inAdj.removeEdge(to, from)
 }
 
+// RemoveNode removes a node entirely from a DirGraph such that
+// no edges exist between it an any other node
 func (dg *DirGraph) RemoveNode(node string) {
 	if nbrs, ok := dg.GetInNeighbors(node); ok {
 		for _, n := range nbrs {
@@ -41,18 +47,22 @@ func (dg *DirGraph) RemoveNode(node string) {
 	}
 }
 
+// PrintAdj displays a DirGraph's adjacency structure as a map of source nodes to target nodes
 func (dg *DirGraph) PrintAdj() {
 	dg.PrintOutAdj()
 }
 
+// PrintOutAdj displays a DirGraph's outgoing adjacency structure
 func (dg *DirGraph) PrintOutAdj() {
 	dg.outAdj.print()
 }
 
+// PrintInAdj displays a DirGraph's incoming adjacency structure
 func (dg *DirGraph) PrintInAdj() {
 	dg.inAdj.print()
 }
 
+// GetNodes gets a slice of all nodes in a DirGraph
 func (dg *DirGraph) GetNodes() (nodes []string) {
 	set := map[string]struct{}{}
 	for node := range dg.outAdj {
@@ -70,18 +80,22 @@ func (dg *DirGraph) GetNodes() (nodes []string) {
 	return nodes
 }
 
+// GetNeighbors gets a slice of nodes that have an edge to them from a specified node
 func (dg *DirGraph) GetNeighbors(node string) (nbrs []string, found bool) {
 	return dg.GetOutNeighbors(node)
 }
 
+// GetOutNeighbors gets a slice of nodes that have an edge to them from a specified node
 func (dg *DirGraph) GetOutNeighbors(node string) (nbrs []string, found bool) {
 	return dg.outAdj.getNeighbors(node)
 }
 
+// GetInNeighbors gets a slice of nodes that have an edge from them to a specified node
 func (dg *DirGraph) GetInNeighbors(node string) (nbrs []string, found bool) {
 	return dg.inAdj.getNeighbors(node)
 }
 
+// GetTotalDegree calculates the sum of weights of all edges from and to a node
 func (dg *DirGraph) GetTotalDegree(node string) (deg float64, found bool) {
 	outDeg, ok := dg.GetOutDegree(node)
 	if !ok {
@@ -94,6 +108,7 @@ func (dg *DirGraph) GetTotalDegree(node string) (deg float64, found bool) {
 	return inDeg + outDeg, true
 }
 
+// GetOutDegree calculates the sum of weights of all edges from a node
 func (dg *DirGraph) GetOutDegree(node string) (deg float64, found bool) {
 	nbrs, ok := dg.GetOutNeighbors(node)
 	if !ok {
@@ -107,6 +122,7 @@ func (dg *DirGraph) GetOutDegree(node string) (deg float64, found bool) {
 	return deg, true
 }
 
+// GetInDegree calculates the sum of weights of all edges to a node
 func (dg *DirGraph) GetInDegree(node string) (deg float64, found bool) {
 	nbrs, ok := dg.GetInNeighbors(node)
 	if !ok {
@@ -120,10 +136,12 @@ func (dg *DirGraph) GetInDegree(node string) (deg float64, found bool) {
 	return deg, true
 }
 
+// HasEdge returns true if an edge exists from a node to another node, false otherwise
 func (dg *DirGraph) HasEdge(from string, to string) bool {
 	return dg.outAdj.hasEdge(from, to)
 }
 
+// GetEdgeWeight gets the weight of an edge from a node to another node if the edge exists
 func (dg *DirGraph) GetEdgeWeight(from string, to string) (weight float64, found bool) {
 	return dg.outAdj.getEdgeWeight(from, to)
 }
--- graph/undirected.go
+++ graph/undirected.go
@@ -1,10 +1,12 @@
 package graph
 
+// Graph is a symmetric adjacency map representation of an undirected graph
 type Graph struct {
 	Name string
 	adj  dirAdj
 }
 
+// NewGraph creates a new undirected graph
 func NewGraph(name string) *Graph {
 	return &Graph{
 		Name: name,
@@ -12,6 +14,7 @@ func NewGraph(name string) *Graph {
 	}
 }
 
+// AddEdge adds an edge between two nodes with an optional weight that defaults to 1.0
 func (g *Graph) AddEdge(from string, to string, weight ...float64) {
 	wgt := 1.0
 	if len(weight) > 0 {
@@ -21,11 +24,14 @@ func (g *Graph) AddEdge(from string, to string, weight ...float64) {
 	g.adj.addEdge(to, from, wgt)
 }
 
+// RemoveEdge removes an edge between two nodes
 func (g *Graph) RemoveEdge(from string, to string) {
 	g.adj.removeEdge(from, to)
 	g.adj.removeEdge(to, from)
 }
 
+// RemoveNode removes a node entirely from a Graph such that
+// no edges exist between it an any other node
 func (g *Graph) RemoveNode(node string) {
 	if nbrs, ok := g.GetNeighbors(node); ok {
 		for _, n := range nbrs {
@@ -34,10 +40,12 @@ func (g *Graph) RemoveNode(node string) {
 	}
 }
 
+// PrintAdj displays a Graph's adjacency structure
 func (g *Graph) PrintAdj() {
 	g.adj.print()
 }
 
+// GetNodes gets a slice of all nodes in a Graph
 func (g *Graph) GetNodes() (nodes []string) {
 	for node := range g.adj {
 		nodes = append(nodes, node)
@@ -45,10 +53,12 @@ func (g *Graph) GetNodes() (nodes []string) {
 	return nodes
 }
 
+// GetNeighbors gets a slice of nodes that are adjacent to a specified node
 func (g *Graph) GetNeighbors(node string) (nbrs []string, found bool) {
 	return g.adj.getNeighbors(node)
 }
 
+// GetDegree calculates the sum of weights of all edges of a node
 func (g *Graph) GetDegree(node string) (deg float64, found bool) {
 	nbrs, ok := g.GetNeighbors(node)
 	if !ok {
@@ -62,10 +72,12 @@ func (g *Graph) GetDegree(node string) (deg float64, found bool) {
 	return deg, true
 }
 
+// HasEdge returns true if an edge exists between two nodes, false otherwise
 func (g *Graph) HasEdge(from string, to string) bool {
 	return g.adj.hasEdge(from, to)
 }
 
+// GetEdgeWeight gets the weight of an edge between two nodes if the edge exists
 func (g *Graph) GetEdgeWeight(from string, to string) (weight float64, found bool) {
 	return g.adj.getEdgeWeight(from, to)
 }
