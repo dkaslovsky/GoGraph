332e244f85c07ca3a5f34c8eee5b1cd3edb74e98
dkaslovsky <dkaslovsky@gmail.com>
2019-05-02 21:35:03 -0600

    DFS and tests

--- search/search.go
+++ search/search.go
@@ -4,18 +4,16 @@ import (
 	n "github.com/dkaslovsky/GoGraph/node"
 )
 
-type neighborGetter interface {
+type hasNodeNeighborGetter interface {
+	HasNode(n.Node) bool
 	GetNeighbors(n.Node) (map[n.Node]float64, bool)
 }
 
 // DFS performs a depth first search starting at a specified node
-func DFS(g neighborGetter, node n.Node) (nodes []n.Node) {
+func DFS(g hasNodeNeighborGetter, node n.Node) []n.Node {
 
-	// add HasNode to graph(s) with tests
-	// add HasNode to interface and rename
-	// return error here?
 	if !g.HasNode(node) {
-		return nodes
+		return []n.Node{}
 	}
 
 	s := n.NewNodeStack()
--- search/search_test.go
+++ search/search_test.go
@@ -9,44 +9,134 @@ import (
 	n "github.com/dkaslovsky/GoGraph/node"
 )
 
-func setupEmptyGraph() *graph.DirGraph {
+func setupEmptyGraph() *graph.Graph {
+	g, _ := graph.NewGraph("empty")
+	return g
+}
+
+func setupEmptyDirGraph() *graph.DirGraph {
 	g, _ := graph.NewDirGraph("empty")
 	return g
 }
 
-func setupGraph() *graph.DirGraph {
+func setupGraph() *graph.Graph {
+	g, _ := graph.NewGraph("no loops")
+	g.AddEdge("a", "z")
+	g.AddEdge("a", "b")
+	g.AddEdge("b", "c")
+	g.AddEdge("c", "d")
+	g.AddEdge("c", "e")
+	g.AddEdge("g", "h")
+	g.AddEdge("h", "i")
+	g.AddEdge("i", "i")
+	return g
+}
+
+func setupDirGraph() *graph.DirGraph {
 	g, _ := graph.NewDirGraph("no loops")
 	g.AddEdge("a", "z")
 	g.AddEdge("a", "b")
 	g.AddEdge("b", "c")
 	g.AddEdge("c", "d")
 	g.AddEdge("c", "e")
+	g.AddEdge("g", "h")
+	g.AddEdge("h", "i")
+	g.AddEdge("i", "i")
 	return g
 }
 
-func TestDFS(t *testing.T) {
+func TestDFS_UndirectedGraph(t *testing.T) {
+	tests := map[string]struct {
+		g             *graph.Graph
+		start         n.Node
+		expectedFound []n.Node
+	}{
+		"empty graph, undirected graph": {
+			g:             setupEmptyGraph(),
+			start:         "a",
+			expectedFound: []n.Node{},
+		},
+		"starting at root, undirected graph": {
+			g:             setupGraph(),
+			start:         "a",
+			expectedFound: []n.Node{"a", "b", "c", "d", "e", "z"},
+		},
+		"starting below root, undirected graph": {
+			g:             setupGraph(),
+			start:         "b",
+			expectedFound: []n.Node{"a", "b", "c", "d", "e", "z"},
+		},
+		"starting from non-existent node, undirected graph": {
+			g:             setupGraph(),
+			start:         "x",
+			expectedFound: []n.Node{},
+		},
+		"starting from terminal node, undirected graph": {
+			g:             setupGraph(),
+			start:         "z",
+			expectedFound: []n.Node{"a", "b", "c", "d", "e", "z"},
+		},
+		"starting from node in component with self loop, undirected graph": {
+			g:             setupGraph(),
+			start:         "h",
+			expectedFound: []n.Node{"g", "h", "i"},
+		},
+		"starting from terminal node with self loop in component, undirected graph": {
+			g:             setupGraph(),
+			start:         "i",
+			expectedFound: []n.Node{"g", "h", "i"},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			found := DFS(test.g, test.start)
+			assert.ElementsMatch(t, found, test.expectedFound)
+		})
+	}
+}
+
+func TestDFS_DirectedGraph(t *testing.T) {
 	tests := map[string]struct {
 		g             *graph.DirGraph
 		start         n.Node
 		expectedFound []n.Node
 	}{
-		"empty graph": {
-			g:             setupEmptyGraph(),
+		"empty graph, directed graph": {
+			g:             setupEmptyDirGraph(),
 			start:         "a",
 			expectedFound: []n.Node{},
 		},
-		"starting at root": {
-			g:             setupGraph(),
+		"starting at root, directed graph": {
+			g:             setupDirGraph(),
 			start:         "a",
 			expectedFound: []n.Node{"a", "b", "c", "d", "e", "z"},
 		},
-		"starting below root": {
-			g:             setupGraph(),
+		"starting below root, directed graph": {
+			g:             setupDirGraph(),
 			start:         "b",
 			expectedFound: []n.Node{"b", "c", "d", "e"},
 		},
-		//"starting at root in graph with self loops"
-		//"starting below root in graph with self loops"
+		"starting from non-existent node, directed graph": {
+			g:             setupDirGraph(),
+			start:         "x",
+			expectedFound: []n.Node{},
+		},
+		"starting from terminal node, directed graph": {
+			g:             setupDirGraph(),
+			start:         "z",
+			expectedFound: []n.Node{"z"},
+		},
+		"starting from node in component with self loop, directed graph": {
+			g:             setupDirGraph(),
+			start:         "h",
+			expectedFound: []n.Node{"h", "i"},
+		},
+		"starting from terminal node with self loop in component, directed graph": {
+			g:             setupDirGraph(),
+			start:         "i",
+			expectedFound: []n.Node{"i"},
+		},
 	}
 
 	for name, test := range tests {
